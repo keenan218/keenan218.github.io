e5722d59f65e7f088c080b0e4446be70f681b124
Keenan Bratley <keenan.bratley-cic@ibm.com>
2025-07-30 16:29:48 +0100

    Adds functionality for working local clock

--- main.js
+++ main.js
@@ -13,6 +13,22 @@ function menuHandler() {
 
 }
 
+//* Local time display
+
+function clockHandler() {
+
+  setInterval(function () {
+    let localTime = new Date();
+
+    document.querySelector('span[data-time=hours]').textContent = localTime.getHours().toString().padStart(2, '0');
+    document.querySelector('span[data-time=minutes]').textContent = localTime.getMinutes().toString().padStart(2, '0');
+    document.querySelector('span[data-time=seconds]').textContent = localTime.getSeconds().toString().padStart(2, '0');
+  }, 1000); // This will update the time every second
+
+}
+
 //* Page load
 
 menuHandler(); // Initialize the menu handler
+clockHandler(); // Initialize the clock handler
+
