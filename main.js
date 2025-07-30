75fea7b2eba26459a9935772ebd115327641fbaf
Keenan Bratley <keenan.bratley-cic@ibm.com>
2025-07-30 16:31:44 +0100

    Adds functionality to greeting section with temperature conversion

--- main.js
+++ main.js
@@ -13,6 +13,44 @@ function menuHandler() {
 
 }
 
+//* Temperature conversion function
+
+function celsiusToFahr(temperature) {
+  let fahr = (temperature * 9 / 5) + 32;
+  return fahr;
+}
+
+//* Greeting section
+
+function greetingHandler() {
+
+  let currentHour = new Date().getHours();
+  let greetingText = currentHour < 12 ? "Good morning!" :
+    currentHour < 19 ? "Good afternoon!" :
+      currentHour < 24 ? "Good evening!" :
+        "Welcome!";
+
+  const weatherCondition = "sunny";
+  const userLocation = "New York";
+  let temperature = 25.397;
+  let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1).toString()}°C outside.`;
+  let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1).toString()}°F outside.`;
+
+  document.querySelector('#greeting').innerHTML = greetingText
+  document.querySelector('p#weather').innerHTML = celsiusText;
+
+  document.querySelector('.weather-group').addEventListener('click', function (e) {
+
+    if (e.target.id == "celsius") {
+      document.querySelector('p#weather').innerHTML = celsiusText;
+    } else if (e.target.id == "fahr") {
+      document.querySelector('p#weather').innerHTML = fahrText;
+    }
+
+  });
+
+}
+
 //* Local time display
 
 function clockHandler() {
@@ -29,6 +67,7 @@ function clockHandler() {
 
 //* Page load
 
-menuHandler(); // Initialize the menu handler
 clockHandler(); // Initialize the clock handler
+menuHandler(); // Initialize the menu handler
+greetingHandler(); // Initialize the greeting handler
 
